33e0b021b0031ab15cfa249570baacdcf1d37751
alimrtyaman <aliyaman0110@gmail.com>
2025-07-31 18:07:04 +0200

    creating user and task entities

---
+++ src/test/java/com/aliyaman/todo_app/entity/Task.java
@@ -0,0 +1,37 @@
+package com.aliyaman.todo_app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "completed")
+    private boolean completed;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "complteted_at")
+    private LocalDateTime completedAt;
+
+
+}
---
+++ src/test/java/com/aliyaman/todo_app/entity/User.java
@@ -0,0 +1,28 @@
+package com.aliyaman.todo_app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+}
